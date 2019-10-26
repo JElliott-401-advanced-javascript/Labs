e2f13b578775d0aaa84339e5a2b3f95366af99e5
pnwjce <elliott.jess@gmail.com>
2019-10-26 15:22:17 -0700

    git issues

--- Lab-08-ExpressRouting-and-ConnectedAPI/index.js
+++ Lab-08-ExpressRouting-and-ConnectedAPI/index.js
@@ -1,12 +1,6 @@
 'use strict';
 
 require('dotenv').config();
-const mongoose = require('mongoose');
 
-const mongooseOptions = {
-    useNewUrlParser:true,
-    useCreateIndex: true,
-};
-mongoose.connect(process.env.MONGODB_URI, mongooseOptions);
-
-require('./src/app.js').start(process.env.PORT);
+// Client >> Server
+require('./lib/server.js').start(process.env.PORT);
